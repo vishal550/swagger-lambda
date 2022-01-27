da92551511c79876dc6b9df75489ab7b7c4a5bac
code_for_vish <vishalpatil550@gmail.com>
2022-01-27 23:14:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const express = require('express')
+const app = express()
+const port = 3000;
+app.use('/', express.static('dist'));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
